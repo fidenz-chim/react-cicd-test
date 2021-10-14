03946b17a854d85991cc08718c8b088f56468fa5
Chim Himidumage <chim@cmbp.local>
2021-10-14 21:45:30 +0530

    update stg 01

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload - dev -01.
+          Edit <code>src/App.js</code> and save to reload - stg -01.
         </p>
         <a
           className="App-link"
